e6a9731d5c49243d5ec7f402eb1532cd9fcc0983
charlie <e_rrol@163.com>
2019-11-22 17:50:01 +0800

    update

---
+++ assets/js/30.b6faf526.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[30],{230:function(t,s,e){"use strict";e.r(s);var n=e(0),i=Object(n.a)({},(function(){var t=this.$createElement,s=this._self._c||t;return s("ContentSlotsDistributor",{attrs:{"slot-key":this.$parent.slotKey}},[s("ol",[s("li",[this._v("手动创建permission权限文件，根据用户所属的权限进行路由拦截")])])])}),[],!1,null,null,null);s.default=i.exports}}]);
