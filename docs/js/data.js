7bb6f8e3bf0ac457015a78e7065a07c37d3bc1d3
charliegogh <e_rrol@163.com>
2020-12-29 17:36:03 +0800

    update

--- docs/js/data.js
+++ docs/js/data.js
@@ -1,4 +1,8 @@
-const data2 ={
-    name:'123'
-}
-console.log(Object.getOwnPropertyNames(data2));
+let arr=[ {name:"soina",value:'122'},
+    {name:"amy",value:'123'},
+    {name:"foina",value:'124'}]
+let obj={}
+arr.map((e,item)=>{
+    obj[e.name]=e
+})
+console.log(obj);
