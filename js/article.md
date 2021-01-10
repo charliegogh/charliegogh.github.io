19837b1cef758ed7ace4a96d1df2f34d51eca5d3
charlie <e_rrol@163.com>
2021-01-10 23:55:43 +0800

    update

--- js/article.md
+++
@@ -1,4 +0,0 @@
-# article
-[JavaScript 中， 5 种增加代码可读性的最佳实践](https://juejin.im/post/5dddbbc3f265da05b44bcf96)
-
-
--- js/技巧/app.js
+++
@@ -1,6 +0,0 @@
-// 数组转对象
-let arr = ["value1","value2","value3"]
-let arrObject = {...arr}
-console.log(arrObject)
-
-
